e9d406c1568036b3c5191c51e20402be182650ba
Alina <alinapolodmi@mail.ru>
2023-09-29 15:30:43 +0200

    ДЗ 28.09.23

---
+++ Task 66/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int SumElements(int m, int n)
+{   
+    if (m > n) return 0;
+    return m + SumElements(m + 1, n);
+}
+
+void PrintSumElements(int m, int n)
+{
+    if (m >= n) 
+    {
+        Console.WriteLine($"Число {m} должно быть меньше чем {n}");
+    }
+    else Console.WriteLine(SumElements(m, n));
+}
+PrintSumElements(1,15);
---
+++ Task 68/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+
+
+int FunctionAckermann(int m, int n)
+{   int func = 0;
+    if (m==0) func = n + 1;
+    if (m > 0 && n==0) func = FunctionAckermann(m-1,1);
+    if (m > 0 && n > 0) func = FunctionAckermann(m-1,FunctionAckermann(m, n-1));
+    return func;
+}
+
+void PrintSumElements(int m, int n)
+{
+    if (m < 0 || n < 0) 
+    {
+        Console.WriteLine($"Числа должны быть больше 0 и m не больше 3");
+    }
+    else if (m > 3) Console.WriteLine($"Число m должно быть от 0 до 3");
+    else if (m ==3 && n > 10) Console.WriteLine($"Раз число m равно 3, n должно быть не больше 10");
+    else Console.WriteLine(FunctionAckermann(m, n));
+}
+PrintSumElements(1,10000);
